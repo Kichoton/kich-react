281e88d10c77273d4a5ab3991d65f1ba96556822
Theo Saez <theo.saez@hotmail.fr>
2024-01-10 22:01:14 -0500

    upgrade network page

--- src/components/Network.js
+++ src/components/Network.js
@@ -4,7 +4,7 @@ import logo_tiktok from '../assets/logo_networks/tiktok-logo.png'
 import logo_insta from '../assets/logo_networks/instagram.png'
 import logo_x from '../assets/logo_networks/x-logo.png'
 import logo_twitch from '../assets/logo_networks/twitch.png'
-import logo_kick from '../assets/logo_networks/kick.png'
+import logo_threads from '../assets/logo_networks/threads.svg'
 import logo_discord from '../assets/logo_networks/discord.svg'
 
 import '../style/Network.css'
@@ -24,6 +24,7 @@ export function Network(){
     {
       index: 0,
       title: 'Tiktok',
+      pseudo: '@kichoton',
       logo: logo_tiktok,
       url: 'https://www.tiktok.com/@kichoton',
       color: '#040404'
@@ -31,34 +32,39 @@ export function Network(){
     {
       index: 1,
       title: 'Insta',
+      pseudo: '@theosaez_',
       logo: logo_insta,
       url: 'https://www.instagram.com/theosaez_/',
       color: 'linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)'
     },
     {
       index: 2,
+      title: 'Thread',
+      pseudo : '@theosaez_',
+      logo: logo_threads,
+      url: 'https://www.threads.net/@theosaez_',
+      color: '#000'
+    },
+    {
+      index: 3,
       title: 'X',
+      pseudo: '@kich_oton',
       logo: logo_x,
       url: 'https://twitter.com/kich_oton',
       color: '#111'
     },
     {
-      index: 3,
+      index: 4,
       title: 'Twitch',
+      pseudo: 'kichoton_',
       logo: logo_twitch,
       url: 'https://twitch.tv/kichoton_',
       color: '#6441a5'
     },
     {
-      index: 4,
-      title: 'Kick',
-      logo: logo_kick,
-      url: 'https://kick.com/kichoton',
-      color: '#000'
-    },
-    {
       index: 5,
       title: 'Discord',
+      pseudo: 'kichoton',
       logo: logo_discord,
       url: 'https://discord.gg/KuXbYdVKZC',
       color: '#5865F2'
@@ -74,13 +80,20 @@ export function Network(){
             href={network.url}
             target='_blank'
             rel="noreferrer"
-            className={'network-div'}
+            className='network-div'
             style={{  
               background: network.color
               }}
           >
-            <img className='network-logo' alt={network.title} src={network.logo} ></img>
 
+            <span>
+              <h2>{network.title}</h2>
+            </span>
+            <span>
+              <h3>{network.pseudo}</h3>
+            </span>
+
+            <img className='network-logo' alt={network.title} src={network.logo} ></img>
           </a>
         ))}
 
